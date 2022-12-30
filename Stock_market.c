f7eb90c363f50482e55ff9e19e2218abe8ce952d
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2022-12-30 17:16:41 +0530

    #include<stdio.h> int main() {     int t,a,b;     scanf("%d",&t);     while(t)     {         scanf("%d%d",&a,&b);         if(a>b)         {             printf("LOSS ");         }         else if(a<b)         {             printf("PROFIT ");         }         else         {         printf("NEUTRAL ");         }         t--;     } }

---
+++ Stock_market.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int t,a,b;
+    scanf("%d",&t);
+    while(t)
+    {
+        scanf("%d%d",&a,&b);
+        if(a>b)
+        {
+            printf("LOSS
+");
+        }
+        else if(a<b)
+        {
+            printf("PROFIT
+");
+        }
+        else
+        {
+        printf("NEUTRAL
+");
+        }
+        t--;
+    }
+}
