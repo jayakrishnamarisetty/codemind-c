e3069c767fef9b580adcc5f514ed13f10d60a4fe
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-10-13 21:26:28 +0530

    #include<stdio.h> int main(){     int n,arr[100];     scanf("%d",&n);     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int temp;     for(int i=0;i<n;i++){         for(int j=i+1;j<n;j++){             if(arr[i]>arr[j]){                 temp=arr[i];                 arr[i]=arr[j];                 arr[j]=temp;             }         }     }     for(int i=0;i<n;i++){             printf("%d ",arr[i]);         } }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n,arr[100];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int temp;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+            printf("%d ",arr[i]);
+        }
+}
